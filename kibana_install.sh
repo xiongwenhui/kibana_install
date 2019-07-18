f723dc8f2387383eb2ec557647542a96e977020d
xiongwenhui <591566564@qq.com>
2019-07-18 16:31:33 +0800

    first commit

---
+++ kibana_install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#下载kibana
+wget --no-check-certificate https://artifacts.elastic.co/downloads/kibana/kibana-6.8.1-linux-x86_64.tar.gz
+tar -xzf kibana-6.8.1-linux-x86_64.tar.gz
+mv kibana-6.8.1-linux-x86_64 /usr/local/kibana
+cd /usr/local/kibana/
+
+#安装sentinl
+bin/kibana-plugin install https://github.com/sirensolutions/sentinl/releases/download/tag-6.6.0-0/sentinl-v6.6.1.zip
+
+#修改配置文件
+mv -f /home/kibana_install-master/kibana.yml /usr/local/kibana/config/kibana.yml
+ip=$(/sbin/ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v 172.17.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:")
+sed -i "s/localhost/$ip/g" /usr/local/kibana/config/kibana.yml
+
+#开机启动
+mv /home/kibana_install-master/es /etc/init.d
+cd /etc/init.d
+chmod +x kibana
+chkconfig --add kibana
+
+#启动kibana
+service kibana start
+echo "启动中......."
