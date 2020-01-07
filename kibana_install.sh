178c2b819c0e161f762568df6133a100cbfad62a
xiongwenhui <591566564@qq.com>
2020-01-07 16:59:45 +0800

    Update kibana_install.sh

--- kibana_install.sh
+++ kibana_install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #下载kibana
-wget --no-check-certificate https://artifacts.elastic.co/downloads/kibana/kibana-6.6.1-linux-x86_64.tar.gz
-tar -xzf kibana-6.6.1-linux-x86_64.tar.gz
-mv kibana-6.6.1-linux-x86_64 /usr/local/kibana
+wget --no-check-certificate https://artifacts.elastic.co/downloads/kibana/kibana-7.5.1-linux-x86_64.tar.gz
+tar -xzf kibana-7.5.1-linux-x86_64.tar.gz
+mv kibana-7.5.1-linux-x86_64 /usr/local/kibana
 cd /usr/local/kibana/
 
 #安装sentinl
-bin/kibana-plugin install https://github.com/sirensolutions/sentinl/releases/download/tag-6.6.0-0/sentinl-v6.6.1.zip
+#bin/kibana-plugin install https://github.com/sirensolutions/sentinl/releases/download/tag-6.6.0-0/sentinl-v6.6.1.zip
 
 #修改配置文件
 mv -f /home/kibana_install-master/kibana.yml /usr/local/kibana/config/kibana.yml
